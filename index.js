8f8efbda3e374c8bce6f8f5b4ff0488554a187f3
thong-trainer <leykamthong@Leys-MacBook-Pro.local>
2018-08-16 09:18:50 +0700

    Add Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -12,6 +12,27 @@ app.use(logger('dev'));
 // public folder
 app.use('/public', express.static('./public'));
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:23477');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // passport
 require('./config/passport')(passport);
 
